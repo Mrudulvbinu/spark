39a050ed36abe833b0198e939dd423b4a59a832c
Mrudulvbinu <mrudulbinu@gmail.com>
2025-03-25 10:49:43 +0530

    16...9

--- backend/server.js
+++ backend/server.js
@@ -47,9 +47,9 @@ const registeredhackathonRoutes = require("./routes/hackathonregistrationroutes"
 app.use("/api/registeredhackathon", registeredhackathonRoutes);
 
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
 app.get("*", (req, res) => {
